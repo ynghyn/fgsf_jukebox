1598b2504b53da9e691620668da195e2b99b6925
ynghyn <ynghynk@gmail.com>
2016-10-22 13:25:20 -0700

    setInterval improvement, production configs, display hidden songs from z-index (#18)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,9 +10,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  # Full error reports are enabled and caching is turned off.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
