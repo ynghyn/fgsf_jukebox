48213ecf82d618f951778acf95404d1da2451e0b
ynghyn <ynghynk@gmail.com>
2016-10-26 14:12:14 -0700

    uses album, artist, carousel (#29)

--- app/helpers/juke_helper.rb
+++ app/helpers/juke_helper.rb
@@ -15,12 +15,16 @@ module JukeHelper
   end
 
   def playing_now(song)
-    if song.try(:file) && is_an_effect?(song)
+    if song && is_an_effect?(song)
       'changing tape..'
-    elsif song.try(:file)
-      normalize_file_name(song.file)
+    elsif song
+      hash = song.to_h
+      title = hash[:title] || normalize_file_name(song.file)
+      artist = hash[:artist]
+      album = hash[:album]
+      "#{title}<br/>#{artist}<br/>#{album}".html_safe
     else
-      ''
+      'Please choose a song.'
     end
   end
 
@@ -29,7 +33,7 @@ module JukeHelper
     if current_song(mpd)
       curr_song_in_queue = mpd_queue(mpd).find { |m| m == current_song(mpd) }
       (0...curr_song_in_queue.pos).each do |i|
-        songs << mpd_queue(mpd)[i].file
+        songs << mpd_queue(mpd)[i]
       end
     end
     songs
@@ -40,7 +44,7 @@ module JukeHelper
     if current_song(mpd)
       curr_song_in_queue = mpd_queue(mpd).find { |m| m == current_song(mpd) }
       (curr_song_in_queue.pos+1...mpd_queue(mpd).size).each do |i|
-        songs << mpd_queue(mpd)[i].file
+        songs << mpd_queue(mpd)[i]
       end
     end
     songs
