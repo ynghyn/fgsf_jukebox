1651eceaa2900e5c4083bf5b21e57ce58098109a
ynghyn <ynghynk@gmail.com>
2016-10-28 17:11:33 -0700

    added album jacket to playing-now list view

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,21 @@ Rails.application.routes.draw do
   get 'comments/local_time_refresh', to: 'comments#local_time_refresh'
 
 
+  # Main pages
   get 'juke/control', to: 'juke#index'
+  get 'juke/list', to: 'juke#list'
+
+  # Partials
   get 'juke/playing_now', to: 'juke#playing_now'
   get 'juke/play_button', to: 'juke#play_button'
-  get 'juke/list', to: 'juke#list'
+  get 'juke/album_jacket', to: 'juke#album_jacket'
+  get 'juke/title', to: 'juke#title'
+
+  # Images
+  get 'juke/mp3_image', to: 'juke#mp3_image'
+  get 'juke/construction', to: 'juke#construction'
+
+  # ControlAPI
   get 'juke/add_song', to: 'juke#add_song'
   get 'juke/stop', to: 'juke#stop'
   get 'juke/next', to: 'juke#next'
@@ -16,9 +27,6 @@ Rails.application.routes.draw do
   get 'juke/pause', to: 'juke#pause'
   get 'juke/pause_or_play', to: 'juke#pause_or_play'
   get 'juke/clear', to: 'juke#clear'
-  get 'juke/title', to: 'juke#title'
-  get 'juke/mp3_image', to: 'juke#mp3_image'
-  get 'juke/construction', to: 'juke#construction'
 
   resources :comments
 end
